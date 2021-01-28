f204104a57d11aa07a21ba5d90cbb56e3159227c
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-28 18:04:01 -0300

    Implements ShoppingCart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
-
-import './App.css';
+import ShoppingCart from './Pages/Shopping-cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={ Home } />
+        <Route path="/shopping-cart" component={ ShoppingCart } />
+        <Route exact path="/" component={ Home } />
       </Switch>
     </BrowserRouter>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <div data-testid="shopping-cart-button">
+          <Link to="/shopping-cart">carrinho</Link>
+        </div>
         <label data-testid="home-initial-message" htmlFor="input-search">
           <input type="text" id="input-search" />
           <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
