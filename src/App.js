f643d227339e2fbb0956e280f1ac4747ab1f350d
Tiago <tiagaoalb@gmail.com>
2021-01-28 16:28:30 -0300

    solved issues

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import * as api from './services/api';
+// import * as api from './services/api';
 
 function App() {
   return (
