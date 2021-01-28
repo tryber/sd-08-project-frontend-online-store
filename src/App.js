733748505d32bb0b7357488477382181d840d010
Fabio Ferreira <fabio35547@gmail.com>
2021-01-28 16:29:09 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { React } from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Go Trybe!
+        <Header />
       </div>
     );
   }
