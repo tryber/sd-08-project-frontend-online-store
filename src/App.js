720f87492d1a1e56dba7d78de57a9b66ae342d66
emassaki <massakihirayama@gmail.com>
2021-01-28 17:46:26 -0300

    Added requirement 3 - created link to shopping cart page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
+import ShoppingCart from './ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
+      <Route path="/shopping-cart" component={ ShoppingCart } />
       <Route exact path="/" component={ LandingPage } />
     </BrowserRouter>
   );
--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <Link to="/shopping-cart" data-testid="shopping-cart-button">
+          Carrinho de Compras
+        </Link>
         <h1
           data-testid="home-initial-message"
         >
