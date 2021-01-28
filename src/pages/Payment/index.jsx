6682824560f63fe49632f2d29dd59a6b3e44982c
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-01-28 16:01:40 -0300

    cria estrutura de componentes/paginas

---
+++ src/pages/Payment/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Payment extends React.Component {
+
+}
+
+export default Payment;
---
+++ src/pages/ProductDetails/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+
+}
+
+export default ProductDetails;
