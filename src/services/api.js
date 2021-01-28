680e7b483e42fe4ac1cf737bec63962980ead6f5
josebejohann <josebernardojohann@gmail.com>
2021-01-28 19:54:31 -0300

    [FIX] Corrige a sintaxe de acordo com o ESLint

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ export async function getCategories() {
   try {
     return (
       await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-        .then(response => response.json())
+        .then((response) => response.json())
     );
   } catch (err) {
     console.log(err);
@@ -13,7 +13,7 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   try {
     return (
       await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-        .then(response => response.json())
+        .then((response) => response.json())
     );
   } catch (err) {
     console.log(err);
