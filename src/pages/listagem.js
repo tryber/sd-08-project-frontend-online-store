13ab8eee35981e01eed7c77fc9d80e2a1a14620c
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-01-28 17:03:48 -0300

    Requisito 4

--- src/pages/listagem.js
+++ src/pages/listagem.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as apiConsult from '../services/api';
 
 class Listagem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+
+    this.createAllCategories = this.createAllCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.createAllCategories();
+  }
+
+  async createAllCategories() {
+    const itemList = await apiConsult.getCategories();
+    console.log(itemList);
+    const spanList = itemList.map((data) => (
+      <li
+        key={ data.id }
+        data-testid="category"
+      >
+        { data.name }
+      </li>));
+    this.setState({ categories: spanList });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div>
+      <div className="main">
+        <div className="categorias">
+          <ul>
+            {categories}
+          </ul>
+        </div>
         <label htmlFor="busca" data-testid="home-initial-message">
           <input type="text" id="busca" />
           Digite algum termo de pesquisa ou escolha uma categoria.
