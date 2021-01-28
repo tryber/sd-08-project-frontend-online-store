cdaa5f089204574d5b188dbf00ed98cc4d8228a5
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-28 17:35:53 -0300

    Corrigindo requisito 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
 import SearchBar from './components/SearchBar';
 
 function App() {
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-
   render() {
     return (
       <div data-testid="home-initial-message">
