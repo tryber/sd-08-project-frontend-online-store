e2a0d44f93c04004df3b4ef5a6ea02745504cac7
Seu nome <luiza.dilly@gmail.com>
2021-01-28 17:17:27 -0300

    fix things again

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as api from '../services/api';
+
+class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // value: '',
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((categories) => console.log(categories));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Sales</h1>
+        <label htmlFor="textInput" data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+          <input id="textInput" />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default Home;
