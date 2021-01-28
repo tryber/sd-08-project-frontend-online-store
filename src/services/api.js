c9993e33d1bd27da046ed1cb37d2f3e5a7a11ad4
Tiago <tiagaoalb@gmail.com>
2021-01-28 17:23:05 -0300

    fixed 1st requirement

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((response) => response.json());
+  const result = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories',
+  ).then((res) => res.json());
+  return result;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((response) => response.json());
+  const result = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  ).then((res) = res.json());
+  return result;
 }
