a6ea836f62fbc06af49aa61e3f19c96ffcb26d7d
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-01-28 18:24:15 -0300

    C2 - 1st Req Sent

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,10 @@
 
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((data) => data.json() );
+    .then((data) => data.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((data) => data.json());
 }
