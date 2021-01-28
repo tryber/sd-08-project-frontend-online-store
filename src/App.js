e5bdd06108af079d7277cac3a7b6d3476277772f
Seu nome <luiza.dilly@gmail.com>
2021-01-28 17:52:03 -0300

    add req 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ Home } />
+          <Route exact path="/" component={ Home } />
+          <Route path="/cart" component={ Cart } />
         </Switch>
       </BrowserRouter>
     </div>
