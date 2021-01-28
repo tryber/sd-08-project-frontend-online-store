201307e27596997b972da8bad3b0c9bdb0434dbd
Hugo Andrade Braga <hugobrr@gmail.com>
2021-01-28 16:19:53 -0300

    troca de lado como o CLeberton piloitando

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import * as api from './services/api';
 function App() {
+componentDidMount() {
+  // api.getCategories().then(categories => { console.log(categories) })
+  // console.log(api.getCategories())
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        { console.log(api.getCategories()) }
       </header>
     </div>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
+ const listOfCategorys ='https://api.mercadolibre.com/sites/MLB/categories'
+
 export async function getCategories() {
   // Implemente aqui
+  if(listOfCategorys === 'https://api.mercadolibre.com/sites/MLB/categories'){
+const result = await fetch(listOfCategorys, {method: 'GET'})
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(response => response.categories)
+    return result;
+  }
+
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query ) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+
 }
