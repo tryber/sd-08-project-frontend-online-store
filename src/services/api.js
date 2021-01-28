c7846d7183f090580d690909f014a0141de43a47
emassaki <massakihirayama@gmail.com>
2021-01-28 16:04:34 -0300

    Refactoring code to remove unnecessary code lines

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,11 @@
 export async function getCategories() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const fetchURL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(fetchURL);
   return response.json();
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  let fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-
-  if (!categoryId) fetchURL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  if (!query) fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-
+  const fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const response = await fetch(fetchURL);
   return response.json();
 }
