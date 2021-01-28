1c77d6218831318be85ce04e31d7411ac7f51e42
Victor Breno <victor.brns2@gmail.com>
2021-01-28 18:10:38 -0300

    Completa requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Search from './components/Search';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ Search } />
+      <Switch>
+        <Route path="/shop-cart" component={ ShoppingCart } />
+        <Route path="/" exact component={ Search } />
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ src/components/ShoppingCartIcon.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import shopCartImg from '../carrinho-de-compras.png';
+
+class ShoppingCartIcon extends React.Component {
+  render() {
+    return (
+      <a
+        href="/shop-cart"
+        data-testid="shopping-cart-button"
+      >
+        <img
+          alt="carrinho de compras"
+          className="shop-cart-img"
+          src={ shopCartImg }
+        />
+      </a>
+    );
+  }
+}
+
+export default ShoppingCartIcon;
