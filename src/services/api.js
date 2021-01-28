7da7ec8cc6a495946af85c0c0775e0a0ad5e63fe
Ediberto Oliveira <edibertooliveira@aol.com>
2021-01-28 16:26:01 -0300

    feat: Implement the Mercado Livre api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
+const ENDPOINT = {
+  categories: 'https://api.mercadolibre.com/sites/MLB/categories',
+  query: (categoryId) => `https://api.mercadolibre.com/sites/MLB/search?q=$${categoryId}`,
+};
+
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch(ENDPOINT.categories);
+  const data = await response.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId) {
+  const response = await fetch(ENDPOINT.query(categoryId));
+  const data = await response.json();
+  return data;
 }
