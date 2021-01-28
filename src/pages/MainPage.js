9449190fa53fba5b21bb7a57b7d1e642109c91ec
Mario Pardo <mariio.pardojr@gmail.com>
2021-01-28 15:52:59 -0400

    Requisto 5

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,10 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import InputSearch from '../components/InputSearch';
 
 class MainPage extends React.Component {
   render() {
     return (
-      <section>
-        <label htmlFor="input-text" data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-          <input id="input-text" />
-        </label>
-      </section>
+      <InputSearch />
     );
   }
 }
