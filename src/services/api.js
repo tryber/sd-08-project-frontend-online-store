860d96ef9a21582ce4e2b810edd38751fab1ae64
maym4y <jennim.almeida@gmail.com>
2021-01-28 15:38:48 -0300

    Funções de busca pela api implementadas

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 export async function getCategories() {
-  // Implemente aqui
+  const categorias = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return categorias.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (!query) {
+    const category = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    return category.json();
+  }
+  if (!categoryId) {
+    const search = await fetch(` https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    return search.json();
+  }
+  const resultado = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  return resultado.json();
 }
