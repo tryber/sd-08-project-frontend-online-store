50db596f2a72c402247c3128672b30ffe8eba04a
Bruno Martins <bruno.martins217@gmail.com>
2021-01-28 17:43:01 -0300

    :package: Feat 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,10 @@ class Home extends Component {
   render() {
     return (
       <section>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <input type="text" />
+        <ul>
+          <li data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</li>
+        </ul>
       </section>
     );
   }
