1abd94125dfc6473f78d6240579cbad655dd9d5f
Thiago Ferreira <thiagocristhian@gmail.com>
2021-01-28 17:19:43 -0300

    Requisito 2

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <input type="text" />
+        <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
