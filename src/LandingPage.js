ef118672256f40ad58cb5c6c59a303f63bb5dab3
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-01-28 18:53:14 -0300

    Finish requeriment 04

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import * as api from './services/api';
 
 class LandingPage extends React.Component {
   constructor() {
     super();
-    
+
     this.state = {
       categories: [],
-    }
+    };
+
+    this.getListCategories = this.getListCategories.bind(this);
   }
+
+  componentDidMount() {
+    this.getListCategories();
+  }
+
+  async getListCategories() {
+    const listCategories = await api.getCategories();
+    this.setState({
+      categories: listCategories,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <input type="text" />
@@ -17,6 +33,15 @@ class LandingPage extends React.Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        <div>
+          {
+            categories
+              .map((category) => (
+                <div key={ category.id } data-testid="category">
+                  { category.name }
+                </div>))
+          }
+        </div>
       </div>
     );
   }
