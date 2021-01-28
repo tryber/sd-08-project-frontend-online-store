da54a402ca529d568cdc60feed3ae76e4149d972
Daniel <marcio.daniel@msn.com>
2021-01-28 20:50:42 -0300

    Requisito 02

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Search  from './Search';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Search />
+    );
+  }
+}
+
+export default Header;
