9f562f0973d3a806580f366539b60bf28f1d8846
MuriloBatista <mub4si@gmail.com>
2021-01-28 15:35:58 -0300

    Initial Commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  fetch(url)
+    .then((data) => data.json())
+    .then((api) => console.log(api));
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
