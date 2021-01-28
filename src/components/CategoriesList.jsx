26b407cdcfdb2d5ae97b8f51819e5ba9d7311516
vivianeflowt <vivianeflowt@gmail.com>
2021-01-28 17:02:46 -0300

    up

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function CategoriesList() {
+  // const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    //
+  });
+
+  const handleChangeSearchText = (e) => {
+    setSearchText(e.target.value);
+  };
+
   return (
-    <div>
-      <h1>CategoriesList</h1>
-    </div>
+    <section className="CategoriesList">
+      <div>
+        <label htmlFor="searchText">
+          <input type="Text" name="searchText" onChange={ handleChangeSearchText } />
+        </label>
+      </div>
+      <nav>
+        <ul>
+          <li> Categoria 1</li>
+          <li> Categoria 2</li>
+          <li> Categoria 3</li>
+        </ul>
+      </nav>
+    </section>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import CategoriesList from '../components/CategoriesList';
+
 export default function Home() {
   return (
     <main className="Home">
       <h1>Home</h1>
+      <CategoriesList />
     </main>
   );
 }
