5a2e9ca38402142b69566fcf15762728d37a2796
Tiago <tiagaoalb@gmail.com>
2021-01-28 16:24:17 -0300

    added 1st requirement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as api from './services/api';
+
 function App() {
   return (
     <div className="App">
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((response) => response.json());
+  await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+    .then((response) => response.json());
 }
