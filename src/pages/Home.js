7bcff2b18f24150b837a631c8d51649c0b02bf99
Joao <joaocarlitoneto@gmail.com>
2021-01-28 18:22:56 -0300

    Send Req 03

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import cartIcon from '../cart-icon.png';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <input type="text" />
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <p>Você ainda não realizou uma busca</p>
+        <Link to="/cartcheckout" data-testid="shopping-cart-button">
+          <img src={ cartIcon } alt="cart icon" />
+        </Link>
       </div>
     );
   }
