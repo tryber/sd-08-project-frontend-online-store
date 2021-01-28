ca99b3dc11e12f9f9835fd825b5861df50a4969e
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-28 14:41:08 -0300

    Criaçao da pasta pages, renderizaçao da pagina de listagem inicialmente vazia

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
+import Listagem from './pages/listagem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Listagem } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
