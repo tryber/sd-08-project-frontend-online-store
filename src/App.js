727c2a9d587c1cbf6ffcddf34e2638692290fa38
VitorLimaRios <vitorlimarios999@gmail.com>
2021-01-28 18:09:32 -0300

    git pull origin master

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import Home from './components/Home';
 import './App.css';
 
 class App extends React.Component {
-  initialMessage() {
-    return (
-      <h2
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h2>
-    );
-  }
-
   render() {
     return (
       <Router>
