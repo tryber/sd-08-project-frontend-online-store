545204f9a22ab1be9f8354df68291ae08275d58a
Daniel <marcio.daniel@msn.com>
2021-01-28 20:07:05 -0300

    Tirnado partes desncessarias da das fatchs.

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,9 @@
 export async function getCategories() {
   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((data) => data.json())
-    .then((categoryList) => categoryList);
+    .then((data) => data.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((data) => data.json())
-    .then((productsByCategoryAndQuery) => productsByCategoryAndQuery);
+    .then((data) => data.json());
 }
