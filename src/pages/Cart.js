cb8897eda7f01e05c55f4f6a9d35e63cacb315f0
Cleberton <clebertonfgc@gmail.com>
2021-01-28 19:34:56 -0300

    Requisito 03 parcial

---
+++ src/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Seu carrinho está vazio</h2>
+      </div>
+    );
+  }
+}
+
+export default Cart;
--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import Cart from './Cart';
 
 export default class Header extends React.Component {
+  constructor() {
+    super();
+
+    this.cartButton = this.cartButton.bind(this);
+  }
+
+  cartButton() {
+    console.log('aa');
+  }
+
   render() {
     return (
       <header className="header">
-        <div className="logo-title-container">
-          <h1>LOGO</h1>
-          <h1>NOME DA LOJA</h1>
-        </div>
-        <h2>SLOGAN</h2>
+        <BrowserRouter>
+          <div className="logo-title-container">
+            <h1>LOGO</h1>
+            <h1>NOME DA LOJA</h1>
+          </div>
+          <h2>SLOGAN</h2>
+          <button type="button" onClick={ this.cartButton }> Carrinho </button>
+          <Link
+            to="/cart"
+            component={ Cart }
+            data-testid="shopping-cart-button"
+          >
+            Carrinho
+          </Link>
+        </BrowserRouter>
       </header>
     );
   }
