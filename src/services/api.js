6c0226be2f2843c7e003e40597c7bb9fc8b967a6
emassaki <massakihirayama@gmail.com>
2021-01-28 15:43:20 -0300

    Added requirement 01 - Fetching categories and products from categories and queries

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  let fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+
+  if (!categoryId) fetchURL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  if (!query) fetchURL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+
+  const response = await fetch(fetchURL);
+  return response.json();
 }
