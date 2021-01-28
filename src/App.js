91b85de916c5a1a2685da2d68795335f17b18cac
Seu nome <luiza.dilly@gmail.com>
2021-01-28 16:19:02 -0300

    REQ1 completo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      hello :DD
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
