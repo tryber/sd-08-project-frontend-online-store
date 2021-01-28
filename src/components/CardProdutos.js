1fee336dca5c09ff6b06d630bd2173623ee76db8
TandyFW <tandyfw@gmail.com>
2021-01-28 18:25:33 -0300

    Adicionado datatest produto

--- src/components/CardProdutos.js
+++ src/components/CardProdutos.js
@@ -5,7 +5,7 @@ class CardProdutos extends React.Component {
   render() {
     const { title, thumbnail, price } = this.props;
     return (
-      <div>
+      <div data-testid="product">
         <h2>{ title }</h2>
         <img src={ thumbnail } alt={ title } />
         <p>{ `R$${price}` }</p>
