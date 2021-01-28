a61f8e4d923b97806d47eec2ca7296dac0833bef
Daniel <danieldsrd@gmail.com>
2021-01-28 15:45:03 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Search from './Components/Search'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Trybe
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Search />
+      </div>      
+    )
+  }
 }
 
 export default App;
