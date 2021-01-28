b72c3429ee03d16d6ba73626d465b49a7327bf2d
Cleberton <clebertonfgc@gmail.com>
2021-01-28 17:08:33 -0300

    Funçao getCategories finalizada -00%

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as api from './services/api';
+// import * as api from './services/api';
 
 function App() {
-componentDidMount() {
-  // api.getCategories().then(categories => { console.log(categories) })
-  // console.log(api.getCategories())
-}
-
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>Edit src/App.js and save to reload.</p>
-        { console.log(api.getCategories()) }
+        {/* {api.getCategories().then(categories => { console.log(categories) })} */}
       </header>
     </div>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,11 @@
- const listOfCategorys ='https://api.mercadolibre.com/sites/MLB/categories'
-
 export async function getCategories() {
   // Implemente aqui
-  if(listOfCategorys === 'https://api.mercadolibre.com/sites/MLB/categories'){
-const result = await fetch(listOfCategorys, {method: 'GET'})
-    .then(response => response.json())
-    // .then(data => console.log(data))
-    .then(response => response.categories)
-    return result;
-  }
-
+  const listOfCategorys = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const result = await fetch(listOfCategorys);
+  const response = await result.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-
 }
