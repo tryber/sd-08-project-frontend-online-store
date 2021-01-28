4a2776a9b4c179d13ed03b119793a91ae6c8ff9d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-01-28 19:49:51 -0300

    corrige funçoes do requisito 04

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { getCategories } from '../services/api';
 
 class CategoryList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [{name: 'Aguarde'}],
+    };
+    this.renderCategoryList = this.renderCategoryList.bind(this);
+  }
+
+  componentDidMount() {
+    this.renderCategoryList();
+  }
+
   async renderCategoryList() {
     const categoryList = await getCategories();
     this.setState(() => ({ categories: categoryList }));
