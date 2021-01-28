891c8013a2aba88cd8fc09880bfc0a38fb61e552
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-01-28 17:30:44 -0300

    create search bar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  listItemCart(searchText, onSearchTextChange) {
+    return (
+      <div>
+        <input
+          name="searchText"
+          type="text"
+          value={ searchText }
+          onChange={ onSearchTextChange }
+        />
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    );
+  }
 
+  render() {
+    return (
+      <header>
+        {this.listItemCart(searchText, onSearchTextChange)}
+      </header>
+    );
+  }
 }
 
 export default SearchBar;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SearchBar from '../../components/SearchBar';
 
 class Home extends React.Component {
-
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default Home;
