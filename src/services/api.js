c0a9dc0c9f65d4a6871af12edc00b1ac9904160b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-01-28 15:03:00 -0300

    Correçao API - descomentando parametros

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ export async function getCategories() {
   return endPoint;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const endPoint = await fetch(
     `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
   ).json();
