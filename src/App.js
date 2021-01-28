b2047e2ab4f18a72e3d09a646e5da5d2a3f30e1d
Douglas Cajueiro <dev.douglas.cajueiro@gmail.com>
2021-01-28 15:15:12 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Front-End Store Project
         </a>
       </header>
     </div>
