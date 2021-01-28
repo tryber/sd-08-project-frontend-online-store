39bf6dd7847c650e045e08eb80b26821158ec770
MEianelli <filipelouzano@gmail.com>
2021-01-28 16:58:37 -0300

    Criei sessao de categorias

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import CartIcon from './components/CartIcon';
 import CartPage from './components/CartPage';
 import './App.css';
 import './services/api';
+import TopNavBar from './components/TopNavBar';
+import * as api from './services/api';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <CartIcon />
-      <Switch>
-        <Route path="/shoppingCart" component={ CartPage } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((result) => {
+      this.setState(() => ({ categories: result }));
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <BrowserRouter>
+        <TopNavBar />
+        <ul className="categoriesList">
+          { categories.map(({ name }) => (
+            <li
+              key={ name }
+              className="categoriesItem"
+            >
+              {name}
+            </li>
+          ))}
+        </ul>
+        <Switch>
+          <Route path="/shoppingCart" component={ CartPage } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
