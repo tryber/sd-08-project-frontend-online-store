e8dd236c1038cb207a2d908a886cc06207249c2a
Lucas Nascimento <lucasmvnascimento@gmail.com>
2021-01-28 16:28:15 -0300

    complete req02

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import * as api from './services/api';
+import ProductList from './pages/ProductList';
 
 api.getCategories();
 api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React (Trybe)
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ ProductList } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
