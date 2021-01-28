6230b84e1b2fb9fb1feef9bc6973b03dc27da864
Joao <joaocarlitoneto@gmail.com>
2021-01-28 19:33:41 -0300

    Send Req04

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import cartIcon from '../cart-icon.png';
+import * as api from '../services/api';
+import '../Home.css';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((categories) => (
+      this.setState({
+        categories,
+      })
+    ));
+  }
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
-        <input type="text" />
-        <p data-testid="home-initial-message">
+        <input id="inputText" type="text" />
+        <Link to="/cartcheckout" data-testid="shopping-cart-button">
+          <img src={ cartIcon } alt="cart icon" className="cartIcon" />
+        </Link>
+        <p htmlFor="inputText" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma
           categoria.
         </p>
-        <Link to="/cartcheckout" data-testid="shopping-cart-button">
-          <img src={ cartIcon } alt="cart icon" />
-        </Link>
+        <ul>
+          {categories.map((category) => (
+            <li key={ category.name } data-testid="category">
+              {category.name}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
