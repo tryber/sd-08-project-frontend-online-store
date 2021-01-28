5577e4b44f3e597533e9362d406891eeb4902dc5
Bruno Martins <bruno.martins217@gmail.com>
2021-01-28 19:18:57 -0300

    :checkered_flag: Feat 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
-import LandingPageCart from './LandingPageCart';
+import LandingPageCart from './components/LandingPageCart';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ Home } />
+      <Route exact path="/" component={ Home } />
       <Route path="/shopping-cart" component={ LandingPageCart } />
     </BrowserRouter>
   );
--- src/components/LandingPageCart.jsx
+++ src/components/LandingPageCart.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class LandingPageCart extends Component {
   render() {
     return (
       <section>
-        <Link to="/shopping-cart" />
         <ul>
           <li data-testid="shopping-cart-empty-message">Seu carrinho está vazio</li>
         </ul>
