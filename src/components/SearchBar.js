d7bf3ce460a02dd7c0eec838e0037ec22199eb6d
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-01-29 15:21:50 -0300

    requisito 5, ainda incompleto

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '',
+      apiReturn: [],
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onClickHandler = this.onClickHandler.bind(this);
+    this.listItems = this.listItems.bind(this);
+  }
+
+  onChangeHandler({ target }) {
+    this.setState({ inputValue: target.value });
+  }
+
+  async onClickHandler() {
+    const { inputValue } = this.state;
+    const apiObjectsReturn = (await getProductsFromCategoryAndQuery('', inputValue))
+      .results;
+    console.log(apiObjectsReturn);
+    this.setState({ apiReturn: apiObjectsReturn });
+    // return apiObjectsReturn.length > 0 ? this.listItems() : <li>NÃO ENCONTRADO</li>
+  }
+
+  // listItems() {
+  //     return (apiReturn.map((product) => {
+  //     <li key={ product.id }> { product.id } </li>
+  //     } )
+  // }
+
   emptySearch() {
     return (
       <p data-testid="home-initial-message">
@@ -9,12 +41,32 @@ class SearchBar extends Component {
   }
 
   render() {
+    const { inputValue, apiReturn } = this.state;
     return (
       <>
         <form>
-          <input name="" value="" />
+          <input
+            name="inputValue"
+            value={ inputValue }
+            data-testid="query-input"
+            onChange={
+              this.onChangeHandler
+            }
+          />
+          <button
+            type="button"
+            id="btnBusca"
+            data-testid="query-button"
+            onClick={ this.onClickHandler }
+          >
+            BUTÃO
+          </button>
         </form>
         {this.emptySearch()}
+        <section>
+          <ul>
+          </ul>
+        </section>
       </>
     );
   }
