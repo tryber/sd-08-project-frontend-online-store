25e97ed665193f1d60ba22b3df7c016a0bcc21c3
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-29 17:17:35 -0300

    [FEAT] adicionada a rota para a pagina do carrinho de compras

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ProductsList from './pages/ProductsList';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" component={ ProductsList } exact />
+        <Route path="/shoppingcart" component={ ShoppingCart } exact />
       </Switch>
     </Router>
   );
