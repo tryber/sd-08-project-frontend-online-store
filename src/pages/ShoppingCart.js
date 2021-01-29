625a39861ae5e702164a5c8cb664113b908a1044
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-29 17:23:13 -0300

    [FEAT] adicionados botao de voltar para o home, um titulo na pagina e uma mensagem indicando que o carrinho esta vazio

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ShoppingCart extends React.Component {
   render() {
-    return 0;
+    return (
+      <section>
+        <header>
+          <Link to="/">Back</Link>
+          <h1>Carrinho de compras</h1>
+        </header>
+        <main>
+          <p data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </p>
+        </main>
+      </section>
+    );
   }
 }
 
