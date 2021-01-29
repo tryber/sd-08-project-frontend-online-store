48f4d8f84784b50b8d2e89aefacd1adb59f3db98
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-01-29 08:00:22 -0300

    [Group-6][Req-3]

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+// import ProductList from './components/ProductList';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <ProductList /> */}
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Home { ...props } />
+            ) }
+          />
+          <Route
+            path="/shopping-cart"
+            render={ (props) => (
+              <ShoppingCart { ...props } />
+            ) }
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
