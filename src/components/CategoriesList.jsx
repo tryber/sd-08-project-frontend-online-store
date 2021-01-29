d5c2c6ab642a8efa13d07891e870e4764b2c8925
wend3ll-souza <wend3llsouza@gmail.com>
2021-01-29 20:01:35 -0300

    :rocket: Feat 6 - First Commit

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ProductCard from './ProductCard';
 
 class CategoriesList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: [],
+      value: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { query, value } = this.state;
     const { list } = this.props;
     return (
-      <ol>
-        Categorias:
-        { list.map(({ id, name }) => (
-          <li data-testid="category" key={ id } value={ id }>
-            { name }
-          </li>))}
-        ;
-      </ol>
+      <section>
+        <ol>
+          Categorias:
+          { list.map(({ id, name }) => (
+            <Link
+              to="/productList"
+              onClick={ this.handleClick }
+              data-testid="category"
+              key={ id }
+              value={ id }
+            >
+              { name }
+            </Link>))}
+        </ol>
+        { query.map((item) => <ProductCard key={ item.id } product={ item } />) }
+      </section>
     );
   }
 }
