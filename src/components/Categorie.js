ee6461227a54685cee712426e2644ede6fe09c17
antonioarieiro <sfgfffs@gmail.com>
2021-01-29 09:20:45 -0300

    switch

--- src/components/Categorie.js
+++ src/components/Categorie.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import '../styles/Categories.css';
-// importando as funções da API como api
 
-function Categories(props) {
-  const { onClick, allCats } = props;
-  return (
-    <div className="sidebar">
-      <h4>Categories</h4>
-      {allCats.map(({ name, id }) => (
-        <label htmlFor={name} key={id}>
-          <input
-            type="radio"
-            className="categoria"
-            data-testid="category"
-            key={id}
-            id={id}
-            name="category"
-            onClick={(event) => onClick(event)}
-          />
-          {name}
-        </label>
-      ))}
-    </div>
-  );
+class CategoryList extends React.Component {
+  constructor() {
+    super();
+
+
+    this.Submit = this.Submit.bind(this);
+  }
+
+  Submit(event) {
+    const { onClick } = this.props;
+    onClick(event);
+  }
+
+  render() {
+    const { items } = this.props;
+    const { key, value } = items;
+    return (
+      <div>
+        <input
+          type="radio"
+          name="category"
+          value={ value }
+          id={ key }
+          data-testid="category"
+          onClick={ this.Submit }
+        />
+        <label htmlFor={ key }>{ value }</label>
+      </div>
+    );
+  }
 }
 
-export default Categories;
+export default CategoryList;
