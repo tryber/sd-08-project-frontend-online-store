2af8f69e71b3e72a020247c5f5f74d28a0e8efa3
Lucas Gama <lucasbg@protonmail.com>
2021-01-29 15:51:19 -0300

    Requisito 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,39 @@
 import React from 'react';
+import { getCategories } from '../services/api';
+import Categories from './Categories';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loadingCategory: true,
+      categories: [],
+    };
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    const data = await getCategories();
+    this.setState({
+      loadingCategory: false,
+      categories: data,
+    });
+  }
+
   render() {
+    const { loadingCategory, categories } = this.state;
+
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        { loadingCategory ? <p>Carregando categorias</p>
+          : <Categories categories={ categories } />}
       </div>
     );
   }
