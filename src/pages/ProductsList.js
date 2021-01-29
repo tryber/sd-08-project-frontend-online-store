e5b26c11eda4dc53887f47d05e664d7fe7aa020b
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-01-29 17:20:27 -0300

    [FEAT] adicionados o botao para a pagina do carrinho de compras e a caixa de texto para busca

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductsList extends Component {
   render() {
     return (
       <main>
+        <input type="text" />
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">Cart</Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
