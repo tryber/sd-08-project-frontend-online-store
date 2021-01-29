f147f17e2d320891fd1a2edc34a471fedf536efa
Felipe Segovia <felipe.vsegovia@gmail.com>
2021-01-29 16:27:26 -0300

    Req06 Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Details from './components.js/Details';
 import CartCheckout from './pages/CartCheckout';
 import Home from './pages/Home';
 
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path="/cartcheckout" component={ CartCheckout } />
         <Route exact path="/" component={ Home } />
+        <Route exact path="/details" component={ Details } />
       </Switch>
     </BrowserRouter>
   );
