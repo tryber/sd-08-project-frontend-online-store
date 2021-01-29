f2a187c06b7a64c2fcfda2305f6fa0e140fe0fea
Bruno <brunocmoraes97@gmail.com>
2021-01-29 15:09:21 -0300

    Fix requisito 3. correçao no encapsulamento da tag span

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class PageShoppingCart extends Component {
   render() {
     return (
-      <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+      <>
+        <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+      </>
     );
   }
 }
