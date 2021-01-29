c4ca77129fabad320ec80c6b99f47de53a9579db
MEianelli <filipelouzano@gmail.com>
2021-01-29 15:46:55 -0300

    Arrumamos req 3 e 4, acrescentamos botao, e produtos

--- src/components/ListagemDeProdutos.jsx
+++ src/components/ListagemDeProdutos.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Search from './Search';
+import PropTypes from 'prop-types';
+import CardProducts from './CardProducts';
 
 class ListagemDeProdutos extends React.Component {
   render() {
+    const { products } = this.props;
+
     return (
-      <Search />
+      <div>
+        { products.map((product) => (<CardProducts
+          key={ product.id }
+          product={ product }
+        />)) }
+      </div>
     );
   }
 }
+
+ListagemDeProdutos.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
 
 export default ListagemDeProdutos;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ListagemDeProdutos from './ListagemDeProdutos';
+import Search from './Search';
+
+class MainPage extends React.Component {
+  render() {
+    const { categories, products, onclick } = this.props;
+    return (
+      <div>
+        <Search />
+        <div className="categoriesList" data-testid="category">
+          { categories.map(({ name, id }) => (
+            <button
+              type="button"
+              key={ name }
+              className="categoriesItem"
+              onClick={ () => onclick(id) }
+            >
+              {name}
+            </button>
+          ))}
+        </div>
+        <ListagemDeProdutos products={ products } />
+      </div>
+    );
+  }
+}
+
+MainPage.propTypes = {
+  categories: PropTypes.arrayOf(PropTypes.any).isRequired,
+  products: PropTypes.arrayOf(PropTypes.any).isRequired,
+  onclick: PropTypes.func.isRequired,
+};
+
+export default MainPage;
