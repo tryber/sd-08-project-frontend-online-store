ccf788e4d159ecb98b9600a665851d1b343928fc
Giovanna Betti <giovannabetti@gmail.com>
2021-01-28 22:43:05 -0300

    REQ-04: OK

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import SearchField from './components/SearchField';
 import SearchResult from './components/SearchResult';
+import Categories from './pages/Categories';
 
 function App() {
   return (
     <div className="App">
-      <SearchField />
       <BrowserRouter>
+        <SearchField />
         <Switch>
           <Route exact path="/" component={ SearchResult } />
         </Switch>
+        <Categories />
       </BrowserRouter>
     </div>
   );
