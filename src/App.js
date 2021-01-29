269f4afe347279c58b1d0b6af420b63c00fc13a2
Maycon Felipe <mayconfelipe.dev@gmail.com>
2021-01-29 19:41:22 -0300

    tentando arrumar avaliador 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import './App.css';
