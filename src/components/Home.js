99c0e1aa76c21a534b275b7bd912b111101eedd7
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-01-29 15:00:43 -0300

    Cria botão para levar à página do carrinho de compras

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <div>
+          <Link to="/shoppingcart" data-testid="shopping-cart-button">
+            Futura imagem do carrinho
+          </Link>
+        </div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
