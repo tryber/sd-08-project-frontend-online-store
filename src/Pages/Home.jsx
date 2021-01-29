321f019dc39000e3597f5d45a00d26a9a6e7f625
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-29 17:41:02 -0300

    Implements ProductList

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import * as api from '../services/api';
 
 import CategoriesList from '../Components/Categories-list';
+import ProductList from '../Components/Product-list';
 
 class Home extends React.Component {
   constructor() {
@@ -10,13 +11,29 @@ class Home extends React.Component {
 
     this.state = {
       categories: [],
+      query: '',
+      listProduct: [],
+      clicked: false,
     };
 
     this.fetchCategory = this.fetchCategory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.clickHandle = this.clickHandle.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategory();
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    this.setState({ query: value });
+  }
+
+  async clickHandle() {
+    const { query } = this.state;
+    const listProduct = await api.getProductsFromCategoryAndQuery('', query);
+    this.setState({ listProduct, clicked: true, });
   }
 
   async fetchCategory() {
@@ -25,17 +42,37 @@ class Home extends React.Component {
   }
 
   render() {
-    const { categories } = this.state;
+    const { categories, listProduct, clicked } = this.state;
     return (
       <div>
         <Link to="/shopping-cart" data-testid="shopping-cart-button">carrinho</Link>
-        <label data-testid="home-initial-message" htmlFor="input-search">
-          <input type="text" id="input-search" />
-          <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-        </label>
+        <form>
+          <label data-testid="home-initial-message" htmlFor="input-search">
+            <input
+              data-testid="query-input"
+              type="text"
+              id="input-search"
+              onChange={ this.handleChange }
+            />
+            <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+          </label>
+          <button
+            data-testid="query-button"
+            type="button"
+            onClick={ this.clickHandle }
+          >
+            Pesquisar
+          </button>
+        </form>
         <aside>
           <CategoriesList categories={ categories } />
         </aside>
+        <main>
+          <ProductList
+            listProduct={ listProduct }
+            clicked={ clicked }
+          />
+        </main>
       </div>
     );
   }
