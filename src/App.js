d928036bf986030b97f7074c938ae5f824119fd2
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-01-29 14:06:25 -0300

    Requirement 3 Passed

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import * as api from './services/api';
 import ProductList from './pages/ProductList';
+import ShoppingCart from './pages/ShoppingCart';
 
 api.getCategories();
 api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola');
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ ProductList } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ ProductList } />
+          <Route exact path="/shoppingcart" component={ ShoppingCart } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ProductList extends React.Component {
   render() {
@@ -7,6 +8,7 @@ class ProductList extends React.Component {
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <Link data-testid="shopping-cart-button" to="/shoppingcart">Carrinho</Link>
       </div>
     );
   }
