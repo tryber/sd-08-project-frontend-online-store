0e14b32abf0051e4d9547b0461c5f5b4930ddcce
Cleberton <clebertonfgc@gmail.com>
2021-01-29 15:56:43 -0300

    Requisito-03-concluido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './pages/Header';
 import Cart from './pages/Cart';
@@ -10,17 +10,11 @@ function App() {
   return (
     <main>
       <BrowserRouter>
-      <Header />
+        <Header />
         <Switch>
           <Route exact path="/cart/" component={ Cart } />
           <Route exact path="/" component={ Home } />
         </Switch>
-        <Link
-            to="/cart/"
-            data-testid="shopping-cart-button"
-          >
-            Carrinho
-          </Link>
       </BrowserRouter>
     </main>
   );
