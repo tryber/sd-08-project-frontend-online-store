efb7c3073e19aecc59dad1c01791a0106a662844
victorjunior95 <victorjr.muchacho@gmail.com>
2021-01-29 16:37:50 -0300

    Componentizando SearchBar, ListCategories e ProductList, renderizando na pagina Home

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-class ProductList extends Component {
+class SearchBar extends Component {
   render() {
     return (
       <div className="header">
-        {/* <input type="text" placeholder="Buscar" className="input" />
+        <input type="text" placeholder="Buscar" className="input" />
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1> */}
+        </h1>
       </div>
     );
   }
 }
 
-export default ProductList;
+export default SearchBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import ProductList from '../components/ProductList';
+import * as api from '../services/api';
+
+import SearchBar from '../components/SearchBar';
 import ShoppingCartLink from '../components/ShoppingCartLink';
+import ListCategories from '../components/ListCategories';
+import ProductList from '../components/ProductList';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCategories: [],
+    };
+
+    this.callApi = this.callApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.callApi();
+  }
+
+  callApi() {
+    this.setState(async () => {
+      const categoryArray = await api.getCategories();
+      this.setState({ listCategories: categoryArray });
+    });
+  }
+
   render() {
+    const { listCategories } = this.state;
     return (
       <div>
+        <SearchBar />
         <ShoppingCartLink />
+        <ListCategories categories={ listCategories } />
         <ProductList />
       </div>
     );
