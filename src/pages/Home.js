ed64ece1f6a3a831b5e457d22d01a3cfcada89fd
UmberBn <humberto-bandeira@hotmail.com>
2021-01-29 20:42:17 -0400

    [Group-6][Req-6][Feat]

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import * as api from '../services/api';
 import ShoppingCartLink from '../components/ShoppingCartLink';
-import ListCategories from '../components/ListCategories';
 import ProductList from '../components/ProductList';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listCategories: [],
-    };
-
-    this.callApi = this.callApi.bind(this);
-  }
-
-  componentDidMount() {
-    this.callApi();
-  }
-
-  callApi() {
-    this.setState(async () => {
-      const categoryArray = await api.getCategories();
-      this.setState({ listCategories: categoryArray });
-    });
-  }
-
   render() {
-    const { listCategories } = this.state;
     return (
       <div>
         <ShoppingCartLink />
-        <ListCategories categories={ listCategories } />
         <ProductList />
       </div>
     );
