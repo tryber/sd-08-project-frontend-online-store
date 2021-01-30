f299842721843f50a65468928e3bb01e0e7302fa
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-01-30 11:18:34 -0300

    req5 ok

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -2,21 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ShoppingCartImg from '../shopping-cart.png';
 import '../App.css';
-import CategoriesFilter from './CategoriesFilter';
 
 export default function InputSearch() {
   return (
     <div>
-      <CategoriesFilter />
-      <form>
-        <input
-          type="search"
-          placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
-        />
-      </form>
-      <h3 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h3>
       <Link data-testid="shopping-cart-button" to="/shoppingcart">
         <img
           className="shopping-cart-icon"
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+import * as api from '../services/api';
+
+export default class ProductList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      productList: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async handleClick() {
+    const { search } = this.state;
+    await api.getProductsFromCategoryAndQuery('', search)
+      .then((data) => {
+        this.setState({
+          productList: data.results,
+        });
+      });
+  }
+
+  render() {
+    const { productList } = this.state;
+    return (
+      <div>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+          <input
+            data-testid="query-input"
+            type="text"
+            placeholder="Encontre seu produto"
+            onChange={ this.handleChange }
+          />
+        </h3>
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          PESQUISAR
+        </button>
+        { productList.map((product) => (
+          <ProductCard key={ product.id } product={ product } />))}
+      </div>
+    );
+  }
+}
