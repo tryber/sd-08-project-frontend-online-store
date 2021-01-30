1c0ac9b27834fdf6c682d1770f29fd2c0b256c9a
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-01-30 16:47:08 -0300

    create-button-add-product-cart

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  buttonAddProduct(productInfo, addProductToCart) {
+    return (
+      <button
+        type="button"
+        data-testid="product-add-to-cart"
+        onClick={ () => { addProductToCart(productInfo); } }
+      >
+        Adicionar Produto
+      </button>
+    );
+  }
+
   render() {
-    const { productInfo } = this.props;
+    const { productInfo, addProductToCart } = this.props;
     return (
       <div className="prodContainer" data-testid="product">
         <img src={ productInfo.thumbnail } alt="product img" />
-        <h3>{productInfo.title}</h3>
+        <h3 data-testid="shopping-cart-product-name">{productInfo.title}</h3>
         <h4>{productInfo.price}</h4>
+        {this.buttonAddProduct(productInfo, addProductToCart)}
       </div>
     );
   }
@@ -20,6 +33,7 @@ ProductCard.propTypes = {
     thumbnail: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
   }).isRequired,
+  addProductToCart: PropTypes.func.isRequired,
 };
 
 export default ProductCard;
