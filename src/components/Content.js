3a3422d3181193fa19962883704590aba6e0fc17
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-01-30 04:41:14 -0300

    Agora o App.js cuida das funcoes principais para permitir o prop drilling

---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Listagem from '../pages/listagem';
+import Carrinho from '../pages/carrinho';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ (routerProps) => <Listagem { ...routerProps } { ...this.props } /> }
+        />
+        <Route exact path="/carrinho" component={ Carrinho } />
+      </Switch>
+    );
+  }
+}
+
+export default Content;
