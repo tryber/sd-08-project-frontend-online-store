2f8956308363def0ed91c4bebe5f6bd8f1b82ee0
Daniel <marcio.daniel@msn.com>
2021-01-30 22:25:36 -0300

    tofix-fine-25-home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // categoryID: '',
       queryProduct: '',
       listProducts: [],
     };
