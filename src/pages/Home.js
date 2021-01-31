1ad43789b4c0e2c3f6416deb99d760f31753f67e
Joao <joaocarlitoneto@gmail.com>
2021-01-30 21:42:27 -0300

    Req 06 with problem

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ class Home extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClik = this.handleClik.bind(this);
+    this.handleProducts = this.handleProducts.bind(this);
 
     this.state = {
       categories: [],
@@ -27,9 +28,16 @@ class Home extends React.Component {
     ));
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   async handleClik() {
     const { category, value } = this.state;
-    const product = await (await api.getProductsFromCategoryAndQuery(category, value))
+    const product = (await api.getProductsFromCategoryAndQuery(category, value))
       .results;
     this.setState({
       products: product,
@@ -37,18 +45,12 @@ class Home extends React.Component {
   }
 
   async handleProducts(id) {
-    const { category, value } = this.state;
-    const product = await (await api.getProductsFromCategoryAndQuery(id, value))
+    const { value } = this.state;
+    const category = (await api.getProductsFromCategoryAndQuery(id, value))
       .results;
+    console.log(id);
     this.setState({
-      products: product,
-    });
-  }
-
-  handleChange({ target }) {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
+      products: category,
     });
   }
 
@@ -61,22 +63,22 @@ class Home extends React.Component {
         <Link to="/cartcheckout" data-testid="shopping-cart-button">
           <img src={ cartIcon } alt="cart icon" className="cartIcon" />
         </Link>
+
         <p htmlFor="inputText" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma
           categoria.
+
           <input
             onChange={ this.handleChange }
             name="value"
             data-testid="query-input"
           />
-          <button
-            type="button"
-            data-testid="query-button"
-            onClick={ this.handleClik }
-          >
+
+          <button type="button" data-testid="query-button" onClick={ this.handleClik }>
             Pesquisar
           </button>
         </p>
+
         <ul>
           {categories.map((category) => (
             <li key={ category.id } data-testid="category">
@@ -86,6 +88,7 @@ class Home extends React.Component {
             </li>
           ))}
         </ul>
+
         {products.map((product) => (
           <div key={ product.id } data-testid="product">
             <p>{product.title}</p>
