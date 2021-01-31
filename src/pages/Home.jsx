2900e21bce774d16adc2efc0acef77a2d44397f8
vivianeflowt <vivianeflowt@gmail.com>
2021-01-31 16:03:35 -0300

    init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import CartSlide from '../components/CartSlide';
 import Header from '../components/Header';
-// import SeachBar from '../components/SearchBar';
 import ProductList from '../components/ProductList';
 
 export default function Home() {
