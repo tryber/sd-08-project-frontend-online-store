f45a5037bb3b9f44de440c7716e9b1279fd42d5a
MEianelli <filipelouzano@gmail.com>
2021-01-31 11:34:59 -0300

    Group 22 - Varias funcionalidades no Details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,27 +1,88 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Stars from './Stars';
+import './ProductDetails.css';
 
 class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.add = this.add.bind(this);
+    this.sub = this.sub.bind(this);
+    const { location: { state: {
+      product: { available_quantity: availableQtd } } } } = props;
+    this.state = {
+      quantidade: 1,
+      available: availableQtd,
+      disable: false,
+    };
+  }
+
+  add() {
+    const { available, quantidade } = this.state;
+    let newQtd = quantidade + 1;
+    if (newQtd > available) {
+      newQtd = available;
+      alert(`Produto só possui ${available} no estoque`);
+    }
+    this.setState(() => ({ quantidade: newQtd, disable: false }));
+  }
+
+  sub() {
+    const { quantidade } = this.state;
+    let newQtd = quantidade - 1;
+    if (newQtd <= 0) {
+      newQtd = 0;
+      this.setState(() => ({ disable: true }));
+    }
+    this.setState(() => ({ quantidade: newQtd }));
+  }
+
   render() {
     const { location: { state: { product } } } = this.props;
     const { addCart } = this.props;
-    const { title, thumbnail, price } = product;
+    const { title, thumbnail, price, attributes,
+      available_quantity: availableQtd } = product;
+    const { value_name: condicao } = attributes[1];
+    const { quantidade, disable } = this.state;
 
     return (
-      <div>
-        <h1 data-testid="product-detail-name">{title}</h1>
-        <img src={ thumbnail } alt="item" />
-        <p>{price}</p>
-        <button
-          onClick={
-            () => addCart(product)
-          }
-          type="button"
-          data-testid="product-detail-add-to-cart"
-        >
-          Adicionar ao carrinho
-        </button>
-
+      <div className="detailDiv">
+        <div className="detailProduct">
+          <div className="detailImg">
+            <img src={ thumbnail } alt="item" />
+          </div>
+          <div className="detailText">
+            <h3>Informações</h3>
+            <p className="detailPrice">{`R$ ${price}`}</p>
+            <h1 data-testid="product-detail-name">{title}</h1>
+            <p className="detailInfo">{`Quantidade disponivel: ${availableQtd}`}</p>
+            <p className="detailInfo">{`Condição do Produto: ${condicao}`}</p>
+            <div className="addAndSub">
+              <button type="button" onClick={ this.sub }>-</button>
+              <div>{quantidade}</div>
+              <button type="button" onClick={ this.add }>+</button>
+            </div>
+            <p className="detailInfo">Quantidade</p>
+            <button
+              onClick={ () => addCart(product) }
+              disabled={ disable }
+              className="addToCartButton"
+              type="button"
+              data-testid="product-detail-add-to-cart"
+            >
+              Adicionar ao carrinho
+            </button>
+          </div>
+        </div>
+        <div className="detailReview">
+          <p>Avaliações</p>
+          <Stars />
+          <form action="#">
+            <input type="text" name="email" placeholder="Email" />
+            <textarea type="text" name="Mensagem" placeholder="Avaliação" />
+            <button className="submitBtn" type="button">Enviar</button>
+          </form>
+        </div>
       </div>
     );
   }
@@ -34,6 +95,9 @@ ProductDetails.propTypes = {
         title: PropTypes.string.isRequired,
         thumbnail: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
+        attributes: PropTypes.arrayOf(PropTypes.any).isRequired,
+        available_quantity: PropTypes.number.isRequired,
+        value_name: PropTypes.string,
       }).isRequired,
     }).isRequired,
   }).isRequired,
