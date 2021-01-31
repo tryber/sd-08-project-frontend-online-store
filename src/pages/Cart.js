eebb0880643e1f0bdb9b384064b29c3e50d7935b
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-01-31 16:47:34 -0300

    Requisito 9 - Finalizado

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { FaBoxOpen } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
+import EmptyCart from '../components/EmptyCart';
+import Header from '../components/Header';
 import './Cart.css';
 
 class Cart extends Component {
+  constructor(props) {
+    super(props);
+    let cartProducts = [];
+    if ({}.hasOwnProperty.call(sessionStorage, 'products')) {
+      cartProducts = JSON.parse(sessionStorage.getItem('products'));
+    }
+
+    this.state = {
+      products: cartProducts,
+    };
+  }
+
+  componentDidUpdate() {}
+
   render() {
+    const { products } = this.state;
     return (
-      <div className="cart-container">
-        <FaBoxOpen className="empty-box" />
-        <p data-testid="shopping-cart-empty-message" className="message-cart">
-          Seu carrinho está vazio
-        </p>
+      <div>
+        <Header />
+        <div>
+          <FaShoppingCart className="icon-cart" />
+          <span className="header-cart">Carrinho de Compras</span>
+          {products.length < 1 && <EmptyCart />}
+          <section className="product-cart-container">
+            {products.map((product) => (
+              <div key={ product.title }>
+                <span
+                  className="product-cart-title"
+                  data-testid="shopping-cart-product-name"
+                >
+                  {product.title}
+                </span>
+                <span
+                  className="product-cart-quantity"
+                  data-testid="shopping-cart-product-quantity"
+                >
+                  1
+                </span>
+              </div>
+            ))}
+          </section>
+        </div>
       </div>
     );
   }
