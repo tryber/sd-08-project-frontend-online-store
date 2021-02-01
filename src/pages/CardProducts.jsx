cee2baf1bdc3b38fcf896e916f9af19bf067e7df
Cainan Coutinho <cainan6697@gmail.com>
2021-02-01 14:26:00 -0300

    issues

--- src/pages/CardProducts.jsx
+++ src/pages/CardProducts.jsx
@@ -11,11 +11,15 @@ class CardProducts extends React.Component {
           <div className="card" key={ id } data-testid="product">
             <Link to={ `/${id}` } data-testid="product-detail-link">
               <img src={ thumbnail } alt={ title } />
-              <h2>R${ price }</h2>
+              <h2>
+                R$
+                { price }
+              </h2>
               <p>{ title }</p>
             </Link>
 
             <button
+              type="button"
               className="add-to-cart-button"
               data-testid="product-detail-add-to-cart"
             >
