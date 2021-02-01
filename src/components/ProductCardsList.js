04c3ac17e2d91a22260805877b089e77c6e35e11
Digo-Castro <dygo.castro@gmail.com>
2021-02-01 14:58:24 -0300

    Adiciona component ProductCardsList

---
+++ src/components/ProductCardsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ProductCard from './Product-Card';
+
+export default class ProductCardsList extends React.Component {
+  render() {
+    return (
+      <ProductCard />
+    );
+  }
+}
