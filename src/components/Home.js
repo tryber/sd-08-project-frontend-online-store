d7fcbb35cf4305e221c74f4e174729f021bc9f3d
mateusvictor7 <mateussantiago.victor@gmail.com>
2021-02-01 18:15:10 -0300

    requisito 5

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,45 @@
 import React from 'react';
+import Search from './Search';
 
 class Home extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      busca: '',
+
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
+  produto(busca) {
     return (
-      <form>
+      <div>
         <label htmlFor="busca" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
           <input
             type="text"
             id="busca"
+            name="busca"
+            onChange={ this.handleChange }
+            data-testid="query-input"
           />
         </label>
+        <Search valor={ busca } />
+      </div>
+    );
+  }
+
+  render() {
+    const { busca } = this.state;
+    return (
+      <form>
+        { this.produto(busca) }
       </form>
     );
   }
