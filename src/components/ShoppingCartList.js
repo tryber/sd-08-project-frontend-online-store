dbfe9edc2ef2feaf981a0797ba1e98b5a251b367
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-02-01 18:19:08 -0300

    requisito 8 completo

--- src/components/ShoppingCartList.js
+++ src/components/ShoppingCartList.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ShoppingCartList extends Component {
   render() {
+    const { image, title, price, qtd, key } = this.props;
     return (
-      <span data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </span>
+      <div key={ key }>
+        <img src={ image } alt={ title } />
+        <h2 data-testid="shopping-cart-product-name">{title}</h2>
+        <p>{price}</p>
+        <p data-testid="shopping-cart-product-quantity">
+          Quantidade:
+          {qtd}
+        </p>
+      </div>
+
     );
   }
 }
+
+ShoppingCartList.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  qtd: PropTypes.number.isRequired,
+  key: PropTypes.string.isRequired,
+};
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import * as LocalStorage from '../services/apiLocalStorage';
 
 import ShoppingCartList from '../components/ShoppingCartList';
 
 export default class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cart: LocalStorage.readCart(),
+    };
+  }
+
   render() {
+    const { cart } = this.state;
     return (
       <div>
         <h1>Seu Carrinho de Compras</h1>
-        <ShoppingCartList />
+        <span data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </span>
+
+        {cart.map(({ id, thumbnail, title, price, qtd }) => (<ShoppingCartList
+          key={ id }
+          image={ thumbnail }
+          price={ price }
+          title={ title }
+          qtd={ qtd }
+        />))}
       </div>
     );
   }
