e3dac01dbb3a6a3af97eed34b0365f234260dc2f
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-02-01 12:47:51 -0300

    Exibe o produto

--- src/pages/Produto.js
+++ src/pages/Produto.js
@@ -1,46 +1,38 @@
 import React from 'react';
-import NavBar from '../components/NavBar';
-import { Link } from 'react-router-dom';
-import * as api from '../services/api';
+import PropTypes from 'prop-types';
 
 class Produto extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      product: {title: 'Pequeno Principe, O'},
-    };
-  }
-
-  componentDidMount() {
-    this.fetchProducts();
-  }
-
-  async fetchProducts() {
-    try {
-      const { id } = this.props.match.params;
-      const produto = await api.getProduct(id);
-      this.setState({
-        product: produto,
-      });
-    } catch (error) {
-      return undefined;
-    }
-  }
   render() {
-    const { product } = this.state;
+    const { listOfProducts } = this.props;
+    const { match: { params: { id } } } = this.props;
+    const product = listOfProducts && listOfProducts.filter((prod) => prod.id === id)[0];
     return (
-      <>
-        <header className="header">
-        {/* <NavBar /> */}
-        <Link to="/carrinho" data-testid="shopping-cart-button"><img alt="carrinho" src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" /></Link>
-        </header>
-        <span data-testid='product-detail-name'>
+      <div className="right-content">
+        <span data-testid="product-detail-name">
           { product && product.title }
-          {console.log(product.title)}
         </span>
-      </>
+      </div>
     );
   }
 }
+
+Produto.propTypes = {
+  listOfProducts: PropTypes.arrayOf(PropTypes.shape({})),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+
+};
+
+Produto.defaultProps = {
+  listOfProducts: [{}],
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: '',
+    }),
+  }),
+};
 
 export default Produto;
