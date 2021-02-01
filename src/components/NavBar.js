45e3fc1f39bff610e61508433d489a9f80952572
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-02-01 11:43:42 -0300

    Botao de mostrar filtros em mobile

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,21 @@ import { Link } from 'react-router-dom';
 import NavForm from './NavForm';
 
 class NavBar extends React.Component {
+  mobileShowFilterOptions() {
+    console.log('teste');
+  }
+
   render() {
     return (
-      <header className="header">
-        <nav className="nav">
-          <NavForm { ...this.props } />
-        </nav>
-        <Link to="/carrinho" data-testid="shopping-cart-button"><img alt="carrinho" src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" /></Link>
-      </header>
+      <>
+        <header className="header">
+          <nav className="nav">
+            <NavForm { ...this.props } />
+          </nav>
+          <Link to="/carrinho" data-testid="shopping-cart-button"><img alt="carrinho" src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" /></Link>
+        </header>
+        <div className="filter-options" onClick={ this.mobileShowFilterOptions }> teste </div>
+      </>
     );
   }
 }
