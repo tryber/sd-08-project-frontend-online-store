250683f1f9cd18c88aebdd909d87cb58c0014043
Igor Nascimento <igorln96@gmail.com>
2021-02-01 17:51:10 -0300

    req9

--- src/components/SpecsTemplate.js
+++ src/components/SpecsTemplate.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class SpecsTemplate extends Component {
+  addCart(item) {
+    const productID = `Product${item.id}`;
+    console.log('xablau');
+    localStorage.setItem(productID, JSON.stringify(item));
+    // JSON.parse(localStorage.getItem('product'));
+  }
+
   render() {
     const { itemInfo } = this.props;
     const { thumbnail, title, price } = itemInfo;
@@ -10,6 +17,13 @@ class SpecsTemplate extends Component {
         <img src={ thumbnail } alt="imagem do produto" />
         <h3 data-testid="product-detail-name">{ title }</h3>
         <p>{ price }</p>
+        <button
+          type="submit"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => this.addCart(itemInfo) }
+        >
+          Adicionar ao Carrinho
+        </button>
       </div>
     );
   }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react';
 import ReturnButoon from '../components/ReturnButton';
 
 class PageShoppingCart extends Component {
+  componentDidMount() {
+    this.getKeys();
+  }
+
+  getKeys() {
+    const keysList = [];
+    for (let index = 0; index < localStorage.length; index += 1) {
+      const key = localStorage.key(index);
+      if (key.includes('Product')) {
+        keysList.push(key);
+      }
+    }
+    return keysList;
+  }
+
+  getItems(key) {
+    const product = JSON.parse(localStorage.getItem(key));
+    return (
+      <div key={ product.id }>
+        <img src={ product.thumbnail } alt={ product.title } />
+        <h3 data-testid="shopping-cart-product-name">{ product.title }</h3>
+        <p>{ product.price }</p>
+        <p>
+          Qtd:
+          <span data-testid="shopping-cart-product-quantity"> 1</span>
+        </p>
+      </div>
+    );
+  }
+
   render() {
+    const keysList = this.getKeys();
     return (
       <>
         <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
         <span />
         <ReturnButoon />
-
+        {keysList.map((key) => this.getItems(key))}
       </>
     );
   }
