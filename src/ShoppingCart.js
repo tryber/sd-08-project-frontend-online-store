6d300838c52c146dc604e28e5c4242364839bf9e
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-02-01 12:03:39 -0300

    Implemented buttons to add and remove quantity

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,18 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ShoppingCart extends React.Component {
+  constructor() {
+    super();
+
+    this.addProduct = this.addProduct.bind(this);
+    this.lessProduct = this.lessProduct.bind(this);
+  }
+
+  addProduct(item) {
+    this.setState(() => ({
+      quantity: item.quantity += 1
+    }))
+  }
+
+  lessProduct(item) {
+    this.setState(() => ({
+      quantity: item.quantity -= 1
+    }))
+  }
   render() {
     const { location: { state: { shoppingCart } } } = this.props;
-    return (
-      <div>
+
+    if (!shoppingCart) {
+      return (
         <p data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </p>
+      )
+    }
+
+    return (
+      <div>
+        <Link to="/">Home</Link>
         {shoppingCart.map((item) => (
           <div key={ item.id }>
-            <p data-testid="shopping-cart-product-name">{ item.title }</p>
-            <p data-testid="shopping-cart-product-quantity">{ 1 }</p>
+            <p data-testid="shopping-cart-product-name">{ item.title} </p>
+            <span>Quantidade</span>
+            <button
+              data-testid="product-decrease-quantity"
+              onClick={ () => this.lessProduct(item)}
+            >
+              -
+                </button>
+            <span data-testid="shopping-cart-product-quantity">{ item.quantity }</span>
+            <button
+              data-testid="product-increase-quantity"
+              onClick={ () => this.addProduct(item)}
+            >
+              +
+                </button>
           </div>
         ))}
       </div>
