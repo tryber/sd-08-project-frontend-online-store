59ea723f370217114307b7288ba846ee5f92d158
victorjunior95 <victorjr.muchacho@gmail.com>
2021-02-01 18:29:25 -0300

    group-6-req-9.2

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import ProductCard from '../components/ProductCard';
 import ShoppingCartLink from '../components/ShoppingCartLink';
 import EvaluationForm from '../components/EvaluationForm';
-import EvaluationList from '../components/EvaluatioList';
+import EvaluationList from '../components/EvaluationList';
 
 class ProductDetails extends Component {
   constructor() {
@@ -13,7 +13,10 @@ class ProductDetails extends Component {
       comment: '',
       rating: undefined,
       evaluation: this.loadEvaluation(),
+      onCart: [], // Minha add
     };
+
+    this.addCart = this.addCart.bind(this); // Minha add
   }
 
   updateField = (field, newValue) => {
@@ -30,50 +33,57 @@ class ProductDetails extends Component {
     }));
   }
 
-loadEvaluation = () => ((localStorage.length !== 0)
-  ? JSON.parse(localStorage.getItem('evaluation'))
-  : [])
+  loadEvaluation = () => ((localStorage.length !== 0)
+    ? JSON.parse(localStorage.getItem('evaluation'))
+    : [])
 
-saveEvaluation = () => {
-  const { evaluation } = this.state;
-  localStorage.setItem('evaluation', JSON.stringify(evaluation));
-}
+  saveEvaluation = () => {
+    const { evaluation } = this.state;
+    localStorage.setItem('evaluation', JSON.stringify(evaluation));
+  }
 
-render() {
-  const { location: { state: { productObj } } } = this.props;
-  const { attributes } = productObj;
-  return (
-    <div>
-      <ShoppingCartLink />
-      <div data-testid="product-detail-name">
-        <ProductCard product={ productObj } />
-      </div>
+  addCart(product) {
+    const { onCart } = this.state;
+    this.setState({ onCart: [...onCart, product] });
+  }
+
+  render() {
+    const { onCart } = this.state;
+    const { location: { state: { productObj } } } = this.props;
+    const { attributes } = productObj;
+    return (
       <div>
-        <ul>
-          Especificações Técnicas
-          {attributes.map((attribute) => (
-            <li
-              key={ attribute.id }
-            >
-              {`${attribute.name}: ${attribute.value_name}`}
-            </li>))}
-        </ul>
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-        >
-          Adicionar ao carrinho
-        </button>
+        <ShoppingCartLink onCart={ onCart } />
+        <div data-testid="product-detail-name">
+          <ProductCard product={ productObj } />
+        </div>
+        <div>
+          <ul>
+            Especificações Técnicas
+            {attributes.map((attribute) => (
+              <li
+                key={ attribute.id }
+              >
+                {`${attribute.name}: ${attribute.value_name}`}
+              </li>))}
+          </ul>
+          <button
+            type="button"
+            onClick={ () => this.addCart(productObj) }
+            data-testid="product-detail-add-to-cart"
+          >
+            Adicionar ao carrinho
+          </button>
+        </div>
+        <EvaluationForm
+          updateField={ this.updateField }
+          handleSubmit={ this.handleSubmit }
+          state={ this.state }
+        />
+        <EvaluationList saveEvaluation={ this.saveEvaluation } state={ this.state } />
       </div>
-      <EvaluationForm
-        updateField={ this.updateField }
-        handleSubmit={ this.handleSubmit }
-        state={ this.state }
-      />
-      <EvaluationList saveEvaluation={ this.saveEvaluation } state={ this.state } />
-    </div>
-  );
-}
+    );
+  }
 }
 
 ProductDetails.propTypes = {
@@ -84,6 +94,7 @@ ProductDetails.propTypes = {
       }).isRequired,
     }).isRequired,
   }).isRequired,
+
 };
 
 export default ProductDetails;
