b92686ee58ca355247a7408dac820bd1cbc3fec8
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-01 15:44:36 -0300

    requisito 08 concluído-subindo novamente pra PR

--- src/Components/CardProduto/index.js
+++ src/Components/CardProduto/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+
 export default class CardProduto extends Component {
   render() {
     const { product } = this.props;
