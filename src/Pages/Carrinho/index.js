ac1e6494c3729b46adc98e7019488b28f7d760ab
Arnaelcio <arnaelcio.gomes@gmail.com>
2021-02-01 15:21:12 -0300

    requisito 08 concluído

--- src/Pages/Carrinho/index.js
+++ src/Pages/Carrinho/index.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BotaoCarrinho from '../../Components/BotaoCarrinho';
 
 export default class Carrinho extends Component {
-  constructor() {
-    super();
-
+  constructor(props) {
+    super(props);
+    const { product } = props.location.state;
     this.state = {
-      products: '',
+      product,
     };
   }
 
   render() {
-    const { products } = this.state;
-    if (!products) {
+    const { product } = this.state;
+
+    const { title, price, thumbnail } = product;
+    if (!product) {
       return (
-        <div data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </div>
+        <>
+          <BotaoCarrinho />
+          <div data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </div>
+        </>
       );
     }
+    return (
+      <>
+        <BotaoCarrinho />
+        <img src={ `${thumbnail}` } alt="product" />
+        <p data-testid="shopping-cart-product-name">{title}</p>
+        <p>{price}</p>
+        <div data-testid="shopping-cart-product-quantity">1</div>
+      </>
+    );
   }
 }
+
+Carrinho.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      product: PropTypes.shape({
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        price: PropTypes.number,
+        thumbnail: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
+};
