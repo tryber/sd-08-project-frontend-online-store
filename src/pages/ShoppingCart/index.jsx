9cfda9443cf4a3e640dc65cd26f01c35c3c7ff96
Bruno Pedrosa <67613857+BrunoPDRS@users.noreply.github.com>
2021-02-02 18:25:05 -0300

    [FIX][Corrigindo importações não utilizadas]

--- src/pages/ShoppingCart/index.jsx
+++ src/pages/ShoppingCart/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AiFillPlusCircle,
   AiOutlinePlusCircle,
   AiFillMinusCircle,
   AiOutlineMinusCircle } from 'react-icons/ai';
-
 
 class ShoppingCart extends React.Component {
   constructor() {
