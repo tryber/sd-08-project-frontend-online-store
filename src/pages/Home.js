69bb7b77f9351495a727cd3441a9d149a3ecf2fc
Arthur Nunes <coletivosuburbano@gmail.com>
2021-02-02 15:37:43 -0300

    componentizando itens da home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import cartIcon from '../cart-icon.png';
 import * as api from '../services/api';
 import '../Home.css';
+import IndexInputs from '../components.js/IndexInputs';
+import ShowCategory from '../components.js/ShowCategory';
+import ShowProducts from '../components.js/ShowProducts';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleClik = this.handleClik.bind(this);
+    this.handleClick = this.handleClick.bind(this);
     this.handleProducts = this.handleProducts.bind(this);
 
     this.state = {
@@ -35,7 +36,7 @@ class Home extends React.Component {
     });
   }
 
-  async handleClik() {
+  async handleClick() {
     const { category, value } = this.state;
     const product = (await api.getProductsFromCategoryAndQuery(category, value))
       .results;
@@ -56,50 +57,20 @@ class Home extends React.Component {
 
   render() {
     const { categories, products } = this.state;
+    const { handleProducts, handleChange, handleClick } = this;
 
     return (
       <div>
-        <input id="inputText" type="text" />
-        <Link to="/cartcheckout" data-testid="shopping-cart-button">
-          <img src={ cartIcon } alt="cart icon" className="cartIcon" />
-        </Link>
-        <p htmlFor="inputText" data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma
-          categoria.
-          <input
-            onChange={ this.handleChange }
-            name="value"
-            data-testid="query-input"
-          />
-          <button type="button" data-testid="query-button" onClick={ this.handleClik }>
-            Pesquisar
-          </button>
-        </p>
+        <IndexInputs
+          handleChange={ handleChange }
+          handleClick={ handleClick }
+        />
         <div className="homepage">
-          <div className="show-category">
-            <ul>
-              {categories.map((category) => (
-                <li key={ category.id }>
-                  <Link
-                    to="/details"
-                    data-testid="category"
-                    onClick={ () => this.handleProducts(category.id) }
-                  >
-                    { category.name }
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="show-products">
-            {products.map((product) => (
-              <div key={ product.id } data-testid="product">
-                <p>{product.title}</p>
-                <img src={ `${product.thumbnail}` } alt={ product.title } />
-                <p>{product.price}</p>
-              </div>
-            ))}
-          </div>
+          <ShowCategory
+            categories={ categories }
+            handleProducts={ handleProducts }
+          />
+          <ShowProducts products={ products } />
         </div>
       </div>
     );
