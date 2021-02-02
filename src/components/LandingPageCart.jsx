74aa7635bab99c186bdc169b5049989ca495defa
Bruno Martins <bruno.martins217@gmail.com>
2021-02-02 19:30:51 -0300

    :package: feature 10

--- src/components/LandingPageCart.jsx
+++ src/components/LandingPageCart.jsx
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class LandingPageCart extends Component {
+  constructor() {
+    super();
+    this.increaseAcc = this.increaseAcc.bind(this);
+  }
+
+  increaseAcc() {
+    let { contador } = this.state;
+    this.setState({
+      contador: contador += 1,
+    });
+  }
+
   render() {
     const { location: { cartProduct } } = this.props;
     return (
@@ -11,6 +23,9 @@ class LandingPageCart extends Component {
             <img src={ product.thumbnail } alt={ product.title } />
             <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
             <p data-testid="shopping-cart-product-quantity">1</p>
+            <button onClick={ this.increaseAcc } data-testid="product-increase-quantity" type="button">+</button>
+            <button data-testid="product-decrease-quantity" type="button">-</button>
+            <button type="button">X</button>
           </div>
         ))}
         <div>
