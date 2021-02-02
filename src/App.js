ef58b6b6bffde03f428478f47a6c8421dd58a117
Rafael <rafaeljavaro750@gmail.com>
2021-02-02 18:15:51 -0300

    Cria pagina de Checkout de Pagamento

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import Details from './pages/Details';
+import Payment from './pages/Payment';
+
 import './App.css';
 
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Route exact path="/" component={ Home } />
         <Route path="/shoppingCart" component={ ShoppingCart } />
         <Route path="/details/:id" component={ Details } />
+        <Route path="/Payment" component={ Payment } />
       </Switch>
     </BrowserRouter>
   );
