26235a525f256ca313bb425ad61337564716d712
Maycon Felipe <mayconfelipe.dev@gmail.com>
2021-02-02 16:15:50 -0300

    Adicionanndo os botões de adicionar, remover e subtrair

--- src/components/ShoppingCartList.js
+++ src/components/ShoppingCartList.js
@@ -3,7 +3,16 @@ import PropTypes from 'prop-types';
 
 export default class ShoppingCartList extends Component {
   render() {
-    const { image, title, price, qtd, key, removeItem, addSubButton, id } = this.props;
+    const {
+      image,
+      title,
+      price,
+      qtd,
+      key,
+      removeItem,
+      addSubButton,
+      id,
+    } = this.props;
     return (
       <div key={ key }>
         <img src={ image } alt={ title } />
@@ -13,11 +22,24 @@ export default class ShoppingCartList extends Component {
           Quantidade:
           {qtd}
         </p>
-        <button type="button" onClick={ () => removeItem(id) }>REMOVER</button>
-        <button data-testid="product-increase-quantity" type="button" onClick={ () => addSubButton(id, 'add') }>ADICIONAR</button>
-        <button data-testid="product-decrease-quantity" type="button" onClick={ () => addSubButton(id, 'sub') }>SUBTRAIR</button>
+        <button type="button" onClick={ () => removeItem(id) }>
+          REMOVER
+        </button>
+        <button
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ () => addSubButton(id, 'add') }
+        >
+          ADICIONAR
+        </button>
+        <button
+          data-testid="product-decrease-quantity"
+          type="button"
+          onClick={ () => addSubButton(id, 'sub') }
+        >
+          SUBTRAIR
+        </button>
       </div>
-
     );
   }
 }
