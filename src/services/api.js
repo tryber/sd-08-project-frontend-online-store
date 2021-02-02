a23944edd75952dd386444d7b9733dde539bd275
Erick-Marinho <erickgaves@gmail.com>
2021-02-02 01:59:11 -0300

    Implementando api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,25 @@
 export async function getCategories() {
-  // Implemente aqui
+  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json());
+
+  return request;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (!query) {
+    const categories = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${categoryId}`)
+      .then((response) => response.json());
+
+    return categories;
+  }
+  if (!categoryId) {
+    const search = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+      .then((response) => response.json());
+
+    return search;
+  }
+  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((response) => response.json());
+
+  return result.json();
 }
