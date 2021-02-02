16f3c525f8bdd3c526ab2f8264dd33b687a31bbc
Erick-Marinho <erickgaves@gmail.com>
2021-02-02 02:05:33 -0300

    Corrigindo api.js

--- src/services/api.js
+++ src/services/api.js
@@ -21,5 +21,5 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
     .then((response) => response.json());
 
-  return result.json();
+  return result;
 }
