6036d4011ea9210f755c375d63bbf7ce1234bff5
Erick-Marinho <erickgaves@gmail.com>
2021-02-02 03:55:46 -0300

    IMplementando requisições assíncronas

--- src/services/api.js
+++ src/services/api.js
@@ -1,25 +1,26 @@
-export async function getCategories() {
-  const request = await fetch('https://api.mercadolibre.com/sites/MLB/categories')
-    .then((response) => response.json());
+const erro = 404;
+const failedRequest = 'Falhou a requisição';
+const noResult = 'Não encontrou nenhum resultado';
+const urlCategory = 'https://api.mercadolibre.com/sites/MLB/categories';
+const urlCategoryID = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+const urlAll = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
 
-  return request;
+async function funcFetch(url) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) return new Error(failedRequest);
+      if (response.status === erro) return new Error(noResult);
+      return response.json();
+    });
+}
+
+export async function getCategories() {
+  await funcFetch(urlCategory);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (!query) {
-    const categories = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${categoryId}`)
-      .then((response) => response.json());
-
-    return categories;
-  }
-  if (!categoryId) {
-    const search = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
-      .then((response) => response.json());
-
-    return search;
-  }
-  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
-    .then((response) => response.json());
-
-  return result;
+  if (!query) return funcFetch(urlCategoryID);
+  if (!categoryId) return funcFetch(urlQuery);
+  funcFetch(urlAll);
 }
