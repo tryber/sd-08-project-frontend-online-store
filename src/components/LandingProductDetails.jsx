bba3d92cbdef8fc948caa2c788df313716badbe2
Bruno Martins <bruno.martins217@gmail.com>
2021-02-02 18:54:34 -0300

    :checkered_flag: Feat 9

--- src/components/LandingProductDetails.jsx
+++ src/components/LandingProductDetails.jsx
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ShopButton from './ShopButton';
 
 class LandingProductDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartProduct: [],
+    };
+    this.addCart = this.addCart.bind(this);
+  }
+
+  componentDidUpdate() {
+    const { location: { product: { id } } } = this.props;
+    const { cartProduct } = this.state;
+    localStorage.setItem(`${id}`, JSON.stringify(cartProduct));
+  }
+
+  addCart(product) {
+    const { cartProduct } = this.state;
+    this.setState({ cartProduct: [...cartProduct, product] });
+  }
+
   render() {
+    const { cartProduct } = this.state;
     const { location: { product } } = this.props;
     const { title, thumbnail, price } = product;
     return (
@@ -10,6 +31,14 @@ class LandingProductDetails extends Component {
         <h1 data-testid="product-detail-name">{title}</h1>
         <img src={ thumbnail } alt={ title } />
         <p>{price}</p>
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          onClick={ () => this.addCart(product) }
+        >
+          Adicionar ao Carrinho
+        </button>
+        <ShopButton cartProduct={ cartProduct } />
       </div>
     );
   }
@@ -20,6 +49,7 @@ LandingProductDetails.propTypes = {
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
+    id: PropTypes.string,
   }) }).isRequired,
 };
 
