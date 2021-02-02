9af89d8383ae4190325e8b47e26a6578c5f489ef
Jorge Alves <alvesdjorge@gmail.com>
2021-02-02 17:34:19 -0300

    req 10

--- src/pages/CarrinhoCompras.js
+++ src/pages/CarrinhoCompras.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class CarrinhoCompras extends Component {
   constructor(props) {
@@ -9,11 +8,7 @@ class CarrinhoCompras extends Component {
     this.decreaseItem = this.decreaseItem.bind(this);
     this.removeItem = this.removeItem.bind(this);
     this.state = {
-<<<<<<< HEAD
-      productsTeste: [],
-=======
-      products: undefined,
->>>>>>> c41d11372c9248dd2fe20db393f13daee211d83a
+      products: [],
     };
   }
 
@@ -22,84 +17,51 @@ class CarrinhoCompras extends Component {
   }
 
   async restoreState() {
-<<<<<<< HEAD
     const products = await JSON.parse(localStorage.getItem('itemsCart'));
-    this.setState({ productsTeste: products });
+    if (products) {
+      this.setState({ products });
+    }
   }
 
   removeItem(item) {
-    const { productsTeste } = this.state;
-    const items = JSON.parse(localStorage.getItem('itemsCart') || '[]');
-    const itemsIndex = items.findIndex((element) => element.id === item.id);
-    const flag = -1;
-    if (itemsIndex !== flag) {
-      delete items[itemsIndex];
-    }
-    localStorage.setItem('itemsCart', JSON.stringify(items));
-    productsTeste.find((element) => element.id === item.id);
-    localStorage.removeItem(item.id);
-    this.setState({ productsTeste });
+    const { products } = this.state;
+    const newProducts = products.filter((element) => element.id !== item.id);
+    localStorage.setItem('itemsCart', JSON.stringify(newProducts));
+    this.setState({ products: newProducts });
   }
 
   increaseItem(item) {
-    const { productsTeste } = this.state;
-    const items = JSON.parse(localStorage.getItem('itemsCart') || '[]');
-    const itemsIndex = items.findIndex((element) => element.id === item.id);
-    const flag = -1;
-    if (itemsIndex !== flag) {
-      items[itemsIndex].qtd += 1;
-    }
-    localStorage.setItem('itemsCart', JSON.stringify(items));
-    productsTeste.find((element) => element.id === item.id).qtd += 1;
-    this.setState({ productsTeste });
+    const { products } = this.state;
+    const newProduct = products.find((element) => element.id === item.id);
+    newProduct.qtd += 1;
+    localStorage.setItem('itemsCart', JSON.stringify(newProduct));
+    this.setState({ products: newProduct });
   }
 
   decreaseItem(item) {
-    const { productsTeste } = this.state;
-    const items = JSON.parse(localStorage.getItem('itemsCart') || '[]');
-    const itemsIndex = items.findIndex((element) => element.id === item.id);
-    const flag = -1;
+    const { products } = this.state;
+    const newProduct = products.find((element) => element.id === item.id);
     const flagZero = 0;
-    if (items[itemsIndex].qtd > flagZero) {
-      if (itemsIndex !== flag) {
-        items[itemsIndex].qtd -= 1;
-      }
-      localStorage.setItem('itemsCart', JSON.stringify(items));
-      productsTeste.find((element) => element.id === item.id).qtd -= 1;
-      this.setState({ productsTeste });
+    if (newProduct.qtd > flagZero) {
+      newProduct.qtd -= 1;
+      localStorage.setItem('itemsCart', JSON.stringify(newProduct));
+      this.setState({ products: newProduct });
     }
   }
 
-  render() {
-    const { productsTeste } = this.state;
+  total() {
+    const { products } = this.state;
+    const totalPrice = products.reduce((acc, item) => acc + (item.price * item.qtd), 0);
     return (
-      <div>
-        {productsTeste ? productsTeste.map((item) => (
-=======
-    const itemsCart = await JSON.parse(localStorage.getItem('itemsCart'));
-    if (itemsCart) {
-      this.setState({ products: itemsCart });
-    }
-  }
-
-  removeItem(item) {
-    console.log(item);
-  }
-
-  increaseItem(item) {
-    console.log(item);
-  }
-
-  decreaseItem(item) {
-    console.log(item);
+      <p>{totalPrice}</p>
+    );
   }
 
   render() {
     const { products } = this.state;
     return (
       <div>
-        {products ? products.map((item) => (
->>>>>>> c41d11372c9248dd2fe20db393f13daee211d83a
+        {(products.length !== 0) ? products.map((item) => (
           <div key={ item.title }>
             <button
               type="submit"
@@ -109,7 +71,6 @@ class CarrinhoCompras extends Component {
             </button>
             <p data-testid="shopping-cart-product-name">{ item.title }</p>
             <p>{ item.price }</p>
-<<<<<<< HEAD
             <p data-testid="shopping-cart-product-quantity">{ item.qtd }</p>
             <button
               type="submit"
@@ -127,34 +88,9 @@ class CarrinhoCompras extends Component {
             </button>
           </div>
         )) : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
-        <Link
-          to={
-            { pathname: '/checkout',
-              state: { products: productsTeste } }
-          }
-          data-testid="checkout-products"
-        >
-          Fechar Carrinho
-        </Link>
-=======
-            <button
-              type="submit"
-              data-testid="product-decrease-quantity"
-              onClick={ () => this.decreaseItem(item) }
-            >
-              -
-            </button>
-            <p data-testid="shopping-cart-product-quantity">{ item.qtd }</p>
-            <button
-              type="submit"
-              data-testid="product-increase-quantity"
-              onClick={ () => this.increaseItem(item) }
-            >
-              +
-            </button>
-          </div>
-        )) : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
->>>>>>> c41d11372c9248dd2fe20db393f13daee211d83a
+        {
+          (products.length !== 0) ? this.total() : ''
+        }
       </div>
     );
   }
