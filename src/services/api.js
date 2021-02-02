87351eb5c1491be2f5f6e0da41e6cacae5741ecf
Erick-Marinho <erickgaves@gmail.com>
2021-02-02 14:43:28 -0300

    refatorando api

--- src/services/api.js
+++ src/services/api.js
@@ -6,21 +6,28 @@ const urlCategoryID = `https://api.mercadolibre.com/sites/MLB/search?category=${
 const urlQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
 const urlAll = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
 
-async function funcFetch(url) {
-  await fetch(url)
+export async function getCategories() {
+  const categorie = await fetch(urlCategory)
     .then((response) => {
       if (!response.ok) return new Error(failedRequest);
       if (response.status === erro) return new Error(noResult);
-      return response.json();
+      response = response.json();
     });
+  return categorie;
 }
 
-export function getCategories() {
-  funcFetch(urlCategory);
-}
-
-export function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (!query) funcFetch(urlCategoryID);
-  if (!categoryId) funcFetch(urlQuery);
-  funcFetch(urlAll);
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (!query) {
+    const category = await fetch(urlCategoryID)
+      .then((response) => response.json());
+    return category;
+  }
+  if (!categoryId) {
+    const search = await fetch(urlQuery)
+      .then((response) => response.json());
+    return search;
+  }
+  const result = await fetch(urlAll)
+    .then((response) => response.json());
+  return result;
 }
