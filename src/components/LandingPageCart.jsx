ed4159df937ec8b071cb358b5c01201bcb4b8ad5
Bruno Martins <bruno.martins217@gmail.com>
2021-02-02 21:47:45 -0300

    :checkered_flag: Feat 10

--- src/components/LandingPageCart.jsx
+++ src/components/LandingPageCart.jsx
@@ -2,37 +2,68 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class LandingPageCart extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {};
     this.increaseAcc = this.increaseAcc.bind(this);
+    this.decreaseAcc = this.decreaseAcc.bind(this);
   }
 
-  increaseAcc() {
-    let { contador } = this.state;
-    this.setState({
-      contador: contador += 1,
-    });
+  componentDidMount() {
+    const { location: { cartProduct } } = this.props;
+    cartProduct.map((product) => this.setState({
+      [product.id]: 1,
+    }));
+  }
+
+  increaseAcc(product) {
+    this.setState((prevState) => ({
+      [product.id]: prevState[product.id] + 1,
+    }));
+  }
+
+  decreaseAcc(product) {
+    this.setState((prevState) => ({
+      [product.id]: prevState[product.id] - 1,
+    }));
   }
 
   render() {
     const { location: { cartProduct } } = this.props;
-    return (
-      <section>
-        {cartProduct.map((product, index) => (
-          <div key={ index }>
-            <img src={ product.thumbnail } alt={ product.title } />
-            <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
-            <p data-testid="shopping-cart-product-quantity">1</p>
-            <button onClick={ this.increaseAcc } data-testid="product-increase-quantity" type="button">+</button>
-            <button data-testid="product-decrease-quantity" type="button">-</button>
-            <button type="button">X</button>
-          </div>
-        ))}
+    if (cartProduct.length < 1) {
+      return (
         <div>
           <p data-testid="shopping-cart-empty-message">
             Seu carrinho está vazio
           </p>
-        </div>
+        </div>);
+    }
+    return (
+      <section>
+        {cartProduct.map((product) => {
+          const { [product.id]: acc } = this.state;
+          return (
+            <div key={ product.id }>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <h3 data-testid="shopping-cart-product-name">{product.title}</h3>
+              <p data-testid="shopping-cart-product-quantity">{acc}</p>
+              <button
+                onClick={ () => this.increaseAcc(product) }
+                data-testid="product-increase-quantity"
+                type="button"
+              >
+                +
+              </button>
+              <button
+                data-testid="product-decrease-quantity"
+                onClick={ () => this.decreaseAcc(product) }
+                type="button"
+              >
+                -
+              </button>
+              <button type="button">X</button>
+            </div>);
+        })}
       </section>
     );
   }
