46934e9808d6185be7d120d31d492016d3d90953
Rodrigo de Castro <72473495+Digo-Castro@users.noreply.github.com>
2021-02-05 16:19:13 -0300

    Update Details.js

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from '../components/Rating';
 
 export default class Details extends React.Component {
   constructor() {
