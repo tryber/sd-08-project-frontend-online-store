225cc5ecdea24425313aec9394261820177bd0b1
vivianeflowt <vivianeflowt@gmail.com>
2021-02-05 08:53:02 -0300

    animação

--- src/components/ButtonCart.jsx
+++ src/components/ButtonCart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
+// import styled from 'styled-components';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { actionSlideOpen } from '../store/control.reducer';
@@ -8,14 +8,30 @@ export default function ButtonCart() {
   const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
   const [count, setCount] = useState(0);
+  const [prevc, setPrevc] = useState(0);
+  const [move, setMove] = useState(false);
 
   const checkoutCounter = () => {
     setCount(cart.length);
+    setPrevc(0);
   };
 
   useEffect(() => {
     checkoutCounter();
   }, [cart]);
+
+  useEffect(() => {
+    console.log('val', count, prevc);
+    if (count > 0 && count > prevc) {
+      console.log('anim');
+      setMove(true);
+      setTimeout(() => {
+        console.log('anim end');
+        setMove(false);
+        setPrevc(count);
+      }, 1000);
+    }
+  }, [count, prevc]);
 
   const handleClickCart = () => {
     dispatch(actionSlideOpen());
@@ -26,7 +42,7 @@ export default function ButtonCart() {
     <button
       type="button"
       data-testid="shopping-cart-button"
-      className="cart-button"
+      className={ move ? 'cart-button anim-move' : 'cart-button' }
       onClick={ handleClickCart }
     >
       <img className="cart-image" src="/icon-cart.png" alt="cart" />
