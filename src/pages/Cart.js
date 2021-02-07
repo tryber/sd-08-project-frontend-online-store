c931525f7a9c9f9e9f9eb15d209d9bff7b183640
hugobrr <hugobrr@gmail.com>
2021-02-07 10:22:47 -0300

    Função já ajustada dentro da Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -32,6 +32,49 @@ export default class Cart extends React.Component {
     });
   }
 
+  addProdutc(product = {}) {
+    const { cartList } = this.state;
+    const index = cartList.findIndex((procu) => product.id === procu.id);
+    if (index >= 0) {
+      cartList[index].quantity += 1;
+      cartList[index].productPrice += product.price;
+    } else {
+      cartList.push({ ...product,
+        quantity: 1,
+        productPrice: product.price,
+      });
+    }
+    this.setState({
+      cartList: [...cartList],
+    });
+  }
+
+  removeProdutc(product = {}) {
+    const { cartList } = this.state;
+    if (!product) { return console.log('Não há produto nesse pedido'); }
+    const index = cartList.findIndex((procu) => product.id === procu.id
+      || product.title === procu.title);
+    if (index >= 0) {
+      cartList[index].quantity -= 1;
+      cartList[index].productPrice -= cartList[index].price;
+      if (cartList[index].quantity === 0) {
+        cartList.splice(index, 1);
+      }
+      this.setState({
+        cartList: [...cartList],
+      });
+    } else {
+      console.log(`Produto ${product} não encontrado`);
+    }
+  }
+
+  //  https://www.freecodecamp.org/news/a-quick-intro-to-higher-order-functions-in-javascript-1a014f89c6b/
+  totalPrice() {
+    const { cartList } = this.state;
+    const result = cartList.reduce((total, product) => product.produtoPrice + total, 0);
+    return result;
+  }
+
   render() {
     const { cartList, qnt } = this.state;
     return (
