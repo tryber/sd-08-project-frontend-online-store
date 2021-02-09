8a5717b43e5930fdd4663916dc8f7e73eb64a3ef
Pedro Piquini <piquini.pedro@gmail.com>
2021-02-09 12:23:28 -0300

    Requisito 10 feito, adicionado botoes de adicionar e retirar items do carrinho e alteraçao de preço total

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,6 +3,48 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ShoppingCart extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      cart: [],
+    };
+  }
+
+  handlePropsToState = () => {
+    const { cart } = this.props;
+    this.setState({
+      cart,
+    });
+
+  };
+
+  componentDidMount() {
+    this.handlePropsToState();
+  }
+
+  handleDecreaseQuantity = (event) => {
+    const { cart } = this.state;
+    const allState = this.state;
+    const findItem = cart.map(item => item.id).indexOf(event.target.id);
+    if (allState.cart[findItem].quantity > 1) {
+      allState.cart[findItem].quantity -= 1;
+      allState.cart[findItem].totalPrice = allState.cart[findItem].price * allState.cart[findItem].quantity;
+      this.setState(allState);
+    }
+  };
+
+  handleIncreaseQuantity = (event) => {
+    const { cart } = this.state;
+    const allState = this.state;
+    const findItem = cart.map(item => item.id).indexOf(event.target.id);
+    allState.cart[findItem].quantity += 1;
+    allState.cart[findItem].totalPrice = allState.cart[findItem].price * allState.cart[findItem].quantity;
+    this.setState(allState);
+  };
+
+
   render() {
     const { cart } = this.props;
 
@@ -31,13 +73,25 @@ class ShoppingCart extends React.Component {
 
         {cart.map((item) => (
           <section key={ item.id }>
+            <button type="button">
+              X
+            </button>
             <p data-testid="shopping-cart-product-name">
               {item.title}
             </p>
 
             <img src={ item.thumbnail } alt="Thumbnail" />
 
-            <p data-testid="shopping-cart-product-quantity">1</p>
+            <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
+
+            <button id={ item.id } data-testid="product-decrease-quantity" type="button" onClick={this.handleDecreaseQuantity}>
+              -
+            </button>
+
+            <button id={ item.id } data-testid="product-increase-quantity" type="button" onClick={this.handleIncreaseQuantity}>
+              +
+            </button>
+            <span>R$ {item.totalPrice.toFixed(2)}</span>
           </section>
         ))}
       </main>
