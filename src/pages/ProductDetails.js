937ba5725eef3c11248287e550f675edb2f6681f
Cezene <cezenesn@hotmail.com.br>
2021-02-08 22:18:53 -0300

    2

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import RateProduct from './Components/RateProduct';
+import RateProduct from '../Components/RateProcuct';
 
 class ProductDetails extends React.Component {
   renderDefaultProps() {
